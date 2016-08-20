89642fe8b6f57f2c4999ffebe42ffd560c6d79d9
Jon Whittlestone <jon@howapped.com>
2016-08-20 15:58:37 +0100

    App 7: Delegating up the inheritance hierarchy to the parent's instance method with super()

--- code/jw/7_wizard_app/actors.py
+++ code/jw/7_wizard_app/actors.py
@@ -1,19 +1,6 @@
 import random
 
 
-class Wizard:
-    def __init__(self, name, level):
-        self.name = name
-        self.level = level
-
-    def attack(self, creature):
-        print("The wizard {} attacks {}!".format(
-            self.name, creature.name
-        ))
-
-        my_roll = random.randint(1, 12 ) * self.level
-        creature_roll
-
 class Creature:
     # level
     # name
@@ -21,3 +8,33 @@ class Creature:
     def __init__(self, name, level):
         self.name = name
         self.level = level
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12) * self.level
+
+
+class Wizard(Creature):
+    # def __init__(self, name, level):
+    #     super().__init__(name, level)
+
+    def attack(self, creature):
+        print("The wizard {} attacks {}!".format(
+            self.name, creature.name
+        ))
+
+        my_roll = self.get_defensive_roll()
+        creature_roll = self.get_defensive_roll()
+
+        print("You roll {} ...".format(my_roll))
+        print("The {} rolls {} ...".format(creature.name, creature_roll))
+
+        if my_roll >= creature_roll:
+            print("The wizard has handily TRIUMPHED over {}".format(creature.name))
+            return True;
+        else:
+            print("The wizard has been DEFEATED :(")
+            return False
+
+
+class Dragon:
+    pass
--- code/jw/7_wizard_app/program.py
+++ code/jw/7_wizard_app/program.py
@@ -1,27 +1,29 @@
 import random
 
+import time
 from actors import Wizard, Creature
 
 
 def print_header():
     print('----------------------------------')
-    print('-----       DARK ROOM        -----')
+    print('-----      DARK FOREST       -----')
     print('----------------------------------')
     print('')
 
 
 def game_loop():
     creatures = [
-        Creature('Toad',1),
+        # Creature('Toad',1),
         Creature('Tiger', 10),
-        Creature('Bat', 3),
-        Creature('Dragon', 50),
-        Creature('Evil Wizard', 1000)
+        # Creature('Bat', 3),
+        # Creature('Dragon', 50),
+        Wizard('Evil Wizard', 1000)
     ]
 
     hero = Wizard('Gandalf', 75)
 
     while True:
+        print()
         active_creature = random.choice(creatures)
         print('A {} of level {} has appeared from a dark and foggy forest ...'
               . format(active_creature.name, active_creature.level))
@@ -31,14 +33,22 @@ def game_loop():
         cmd = cmd.lower().strip()
 
         if cmd == 'a':
-            hero.attack(active_creature)
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                time.sleep(5)
         elif cmd == 'r':
-            print('Run Away!')
+            print('The wizard has become unsure of his power and flees')
         elif cmd == 'l':
-            print('Look Around.')
+            print('The wizard takes in the beautiful surroundings and sees ...')
+            for c in creatures:
+                print(' * A {} of level {}' . format(c.name, c.level))
+                time.sleep(1)
         else:
             break
 
+        if not creatures:
+            print('You have defeated all the creatures. Well Done!')
 
 def main():
     print_header()
