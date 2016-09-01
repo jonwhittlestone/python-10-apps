7205c56cda3b8f02f9ec2eaf11d461e9f1381e05
Jon Whittlestone <jon@howapped.com>
2016-09-01 14:53:03 +0100

    App 8: Recursive file searching. Storing meta-data in Named Tuple

--- code/jw/8_file_search/program.py
+++ code/jw/8_file_search/program.py
@@ -1,5 +1,8 @@
 import os
+import collections
 
+SearchResult = collections.namedtuple('SearchResult',
+                                      'file, line, text')
 
 def print_header():
     print('---------------------------------------------')
@@ -7,6 +10,7 @@ def print_header():
     print('---------------------------------------------')
 
 def get_folder_from_user():
+    # /Users/jon/code/playground/python-10-apps/code/jw/8_file_search/books
     folder = input('What folder do you want to search? ')
 
     if not folder or not folder.strip():
@@ -18,11 +22,51 @@ def get_folder_from_user():
     return os.path.abspath(folder)
 
 def get_search_text_from_user():
-    text = input("What are you searching for? [single phrases only]" )
-    return text
+    text = input("What are you searching for? [single phrases only] ")
+    return text.lower()
+
+
+def search_file(filename, search_text):
+    matches = []
+    with open(filename, 'r', encoding='utf-8') as fin:
+        line_num = 0
+        for line in fin:
+            line_num += 1
+            if line.lower().find(search_text) >= 0:
+                m = SearchResult(line=line_num, file=filename, text=line)
+                matches.append(m)
+    return matches
 
 def search_folder(folder, text):
-    print("Would search {} for {}" . format(folder, text))
+    # print("Would search {} for {}" . format(folder, text))
+    all_matches = []
+
+    items = os.listdir(folder)
+    for item in items:
+        full_item = os.path.join(folder, item)
+
+        if item == '.DS_Store':
+            continue
+
+        if os.path.isdir(full_item):
+            matches = search_folder(full_item, text)
+            all_matches.extend(matches)
+        else:
+            matches = search_file(full_item, text)
+            all_matches.extend(matches)
+
+    return all_matches
+
+
+def show_results(matches):
+    print('Found {} matches ..' . format(len(matches)))
+    for m in matches:
+        print("** ----   MATCH   --- **")
+        print('File: ' + m.file)
+        print('Line #: {}' .format(m.line))
+        print('Match: ' + m.text.strip())
+        print()
+
 
 def main():
     print_header()
@@ -36,7 +80,8 @@ def main():
         print("Sorry, we can't search that text")
         return
 
-    search_folder(folder, text)
+    matches = search_folder(folder, text)
+    show_results(matches)
 
 if __name__ == '__main__':
     main()
